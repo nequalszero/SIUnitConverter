cb2058583881384b3fb657d35709d737bf6cff64
nequalszero <nequalszero@gmail.com>
2017-05-23 22:45:18 -0700

    Add another test

--- spec/lib/si_unit_string_spec.rb
+++ spec/lib/si_unit_string_spec.rb
@@ -80,8 +80,9 @@ describe "SIUnitString" do
         let(:string1) { SIUnitString.new("(degree)") }
         let(:string2) { SIUnitString.new("(degree/minute)") }
         let(:string3) { SIUnitString.new("(degree*ha)/minute") }
-        let(:string4) { SIUnitString.new("(degree * ha) / (minute * ° * °) * (\' * \")") }
-        let(:string5) { SIUnitString.new("(degree * (ha)) / (((minute) * °) * °) * (((\' * \")))") }
+        let(:string4) { SIUnitString.new("minute/(degree*ha)") }
+        let(:string5) { SIUnitString.new("(degree * ha) / (minute * ° * °) * (\' * \")") }
+        let(:string6) { SIUnitString.new("(degree * (ha)) / (((minute) * °) * °) * (((\' * \")))") }
 
         it "can parse a string with one unit" do
           expect(string1.parse).to eq({
@@ -104,11 +105,18 @@ describe "SIUnitString" do
           })
         end
 
+        it "can parse another string with three units" do
+          expect(string4.parse).to eq({
+            unit_name: "s/(rad*m^2)",
+            multiplication_factor: (60/(Math::PI/180*10000)).round(14)
+          })
+        end
+
         it "can parse a string with many units and parenthesis groupings" do
           expected_units = "(rad*m^2)/(s*rad*rad)*(rad*rad)"
           expected_factor = (Math::PI/180*10000/60/(Math::PI/180)/(Math::PI/180)*(Math::PI/10800)*(Math::PI/648000)).round(14)
 
-          expect(string4.parse).to eq({
+          expect(string5.parse).to eq({
             unit_name: expected_units,
             multiplication_factor: expected_factor
           })
@@ -118,7 +126,7 @@ describe "SIUnitString" do
           expected_units = "(rad*(m^2))/(((s)*rad)*rad)*(((rad*rad)))"
           expected_factor = (Math::PI/180 * 10000/60/(Math::PI/180)/(Math::PI/180)*(Math::PI/10800)*(Math::PI/648000)).round(14)
 
-          expect(string5.parse).to eq({
+          expect(string6.parse).to eq({
             unit_name: expected_units,
             multiplication_factor: expected_factor
           })
