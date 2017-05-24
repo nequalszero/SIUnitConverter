b6af1d2a13cccef3656c0ade0a85fb41b90fbd07
nequalszero <nequalszero@gmail.com>
2017-05-23 20:44:53 -0700

    Attempt to disable CORS

--- app/controllers/units/si_controller.rb
+++ app/controllers/units/si_controller.rb
@@ -1,6 +1,8 @@
 require_relative '../../../lib/si_unit_string'
 
 class Units::SiController < ApplicationController
+  protect_from_forgery except: :index
+
   def index
     si_unit_string = SIUnitString.new(params[:units])
     si_unit_string.parse
